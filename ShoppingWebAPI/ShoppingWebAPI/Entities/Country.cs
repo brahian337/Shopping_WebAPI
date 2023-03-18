ca0ea09e5481c734820eaa6337b7d10d85c5dbed
Brahian Grajales <brahian337@gmail.com>
2023-03-18 09:12:53 -0500

    New WebAPI Shopping project with new Country Entity

---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Country.cs
@@ -0,0 +1,7 @@
+﻿namespace ShoppingWebAPI.Entities
+{
+    public class Country : Entity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingWebAPI.Entities
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
